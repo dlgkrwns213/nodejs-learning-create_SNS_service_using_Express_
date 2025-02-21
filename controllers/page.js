0635449b2770b710524da8ff25158374720ea55c
이학준 <dlgkrwns213@naver.com>
2025-02-21 17:15:22 +0900

    Define render functions for profile, join, and main pages
    - Set titles and render appropriate views for profile, join, and main pages
    - Initialize an empty array for 'twits' in the main page render

---
+++ controllers/page.js
@@ -0,0 +1,15 @@
+const renderProfile = (req, res) => {
+  res.render("profile", {title: "내 정보 - NodeBird"});
+}
+
+const renderHome = (req, res) => {
+  res.render("join", {title: "회원 가입 - NodeBird"});
+}
+
+const renderMain = (req, res, next) => {
+  const twits = [];
+  res.render("main", {
+    title: "NodeBird",
+    twits,
+  });
+};
